29dcd9970936f0f263db245362f3f0814e0b5e70
douglassqueiroz <douglas.queiroz18@outlook.com>
2022-11-15 18:33:09 -0400

    Create a bigger iteration with the client

--- js/iRock.js
+++ js/iRock.js
@@ -1,20 +1,19 @@
-/*function touchRock() {
+
+function touchRock() {
     var userName = prompt("Qual é o seu nome?");
 
-if (userName) 
-    alert("Prazer em ti conhecer, " + userName + ".");
-    document.getElementById("rockImg").src = "https://cdn.pixabay.com/photo/2020/12/27/20/25/smile-5865209_960_720.png";
-}*/
+    if (userName){
+        alert("Prazer em ti conhecer " + userName + ".");
+        document.getElementById("rockImg").src = "https://i.pinimg.com/originals/df/72/33/df72333c26e2eece5f5a08e51e990edf.png";
+        document.getElementById("text").innerHTML="";
+    }
+    if (userName) {
+        setTimeout("document.getElementById('rockImg').src='https://i.pinimg.com/474x/48/80/9b/48809b84353e9aebef7b6f3d7c3e1263.jpg';document.getElementById('text').innerHTML='Vc me deixou sozinho novamente, clique em mim pra falar comigo dnovo!!!';",10000);
+    }
 
-
-//Caso o cliente clique no emogi, ocorretá a mudança da primeira para a segunda imagem
-function touchRock() {
-    document.getElementById("rockImg").src = "https://cdn.pixabay.com/photo/2020/12/27/20/25/smile-5865209_960_720.png";
+       
 }
 
-//Caso o cliente não clique no emogi pela quantidade de tempo determinada pelo parâmetro setTimeOut, ocorrerá a mudança de emogi e ele ficará triste.
-function finishTime() {
-document.getElementById("rockImg").src = "https://cdn.pixabay.com/photo/2020/12/27/20/25/smile-5865209_960_720.png";
-}
-setTimeout(finishTime, 7000);
+    
+
 
